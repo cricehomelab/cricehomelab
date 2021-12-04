df741c32df70f4b8bece0406f490ed1cc2012c8b
cricehomelab <91029276+cricehomelab@users.noreply.github.com>
2021-12-04 17:01:48 -0500

    Add files via upload

---
+++ AddProfilePathForRoamingUsers.ps1
@@ -0,0 +1,10 @@
+﻿#Import the AD module
+Import-Module ActiveDirectory
+
+#Get all members of the roaming profile group and set the correct network profile path for them. 
+Get-ADGroupMember 'Roaming Profile Users' | 
+    # loop through the users and set their profile path. 
+    ForEach-Object {
+    # Set the profile path for each user in the list. 
+    Set-ADUser -Identity $_.SamAccountName -ProfilePath("\\domainctl-01\RoamingUserProfiles$\" + $_.SamAccountName)
+    }
